3f611177af5bc0038b00cbc715a5e38aa7af785b
Peter Varga <peter.varga.sp@gmail.com>
2021-03-14 11:13:20 +0100

    Code review

--- src/main/java/com/example/demuser/DemUserApplication.java
+++ src/main/java/com/example/demuser/DemUserApplication.java
@@ -6,6 +6,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+// TODO KT 2021-03-14: rename to DemoUserApplication
 public class DemUserApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demuser/controller/UserController.java
+++ src/main/java/com/example/demuser/controller/UserController.java
@@ -2,39 +2,53 @@ package com.example.demuser.controller;
 
 import com.example.demuser.entity.User;
 import com.example.demuser.repository.UserRepository;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+// TODO KT 2021-03-14: rename to UserRestController
+// TODO KT 2021-03-14: create mvc test for the RestController
 @RequestMapping("/api")
 @RestController
-public class UserController {
+public class UserController
+{
     private UserRepository userRepository;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository)
+    {
         this.userRepository = userRepository;
     }
 
+    // TODO KT 2021-03-14:  rename mapping to "/user"
     @GetMapping("/users")
-    public List<User> allUser() {
+    public List<User> allUser()
+    {
         return (List<User>) userRepository.findAll();
     }
+
     @GetMapping("/{id}")
-    public User findById(@PathVariable String id) {
+    public User findById(@PathVariable String id)
+    {
         return userRepository.findById(id).orElseThrow(() -> new UserNotFounException(id));
     }
 
+    // TODO KT 2021-03-14:  rename mapping to "/user"
     @PostMapping("/create")
-    public User create(@RequestBody User user) {
+    public User create(@RequestBody User user)
+    {
         return userRepository.save(user);
     }
 
+    // TODO KT 2021-03-14:  rename mapping to "/user"
     @PutMapping("/modify")
-    public User update(@RequestBody User userToSave) {
+    public User update(@RequestBody User userToSave)
+    {
         Optional<User> userOptional = userRepository.findById(userToSave.getId());
-        if (userOptional.isPresent()) {
+        if (userOptional.isPresent())
+        {
             User user = userOptional.get();
             BeanUtils.copyProperties(userToSave, user, "id");
             userRepository.save(user);
@@ -45,8 +59,12 @@ public class UserController {
 
     }
 
+    // TODO KT 2021-03-14:  rename mapping to "/user"
+    // TODO KT 2021-03-14:  path variable is not defined in the mapping. It can not work like this
     @DeleteMapping("/delete")
-    public void delete(@PathVariable String id) {
+    public void delete(@PathVariable String id)
+    {
+        // TODO KT 2021-03-14: check the status code if UserNotFounException gets thrown. It should be 404
         User user = userRepository.findById(id).orElseThrow(() -> new UserNotFounException(id));
         userRepository.delete(user);
     }
